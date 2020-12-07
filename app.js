d1004033031f43e3a2157d9bca329fa0fbb41639
Howesy <howesybusiness@gmail.com>
2020-12-07 01:48:01 +0000

    Added JSDoc and comments.

--- app.js
+++ app.js
@@ -4,9 +4,16 @@ const {readdir} = require("fs");
 const application = express();
 const specifiedPort = 3000;
 
+//Serve static files contained within the public directory.
+
 application.use(express.static("public"));
 
 handleHTMLDocuments();
+
+/**
+ * Scan and handle GET requests for HTML documents contained within the current directory.
+ * @author Howesy
+ */
 
 function handleHTMLDocuments() {
     readdir(__dirname, function(error, documents) {
@@ -16,11 +23,18 @@ function handleHTMLDocuments() {
     });
 }
 
+/**
+ * Handle GET request to specified HTML document.
+ * @param {string} specifiedPage Desired HTML document to handle a GET request for.
+ */
+
 function handlePageRequest(specifiedPage) {
     application.get(`/${specifiedPage}`, function(request, response) {
         const resolvedPath = resolve(__dirname, specifiedPage);
         response.sendFile(resolvedPath);
     });
 }
+
+//Initialize the express application to listen on our specified port: [3000] and send a conformation message.
 
 application.listen(specifiedPort, () => console.log("Server successfully initialized."));
