3a34a9743a5ea25eb06dffe9a2a3b3fa70538742
Charlie <howesybusiness@gmail.com>
2022-06-28 23:34:47 +0100

    Added "tractualizeViews" function, added comments

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const session = require("express-session");
 const { urlencoded, json } = require("body-parser");
+const { readdir } = require("fs");
+const { join } = require("path");
 const application = express();
 const specifiedPort = 3000;
 
@@ -17,16 +19,33 @@ application.use(
     })
 );
 
-actualizeView("index");
+tractualizeViews();
 
 application.listen(specifiedPort, () => console.log("Server successfully initialized."));
 
+
+//Actualize view into existence, when called upon on the server; will display the view passed.
 function actualizeView(viewName, ...desiredMiddleware) {
     application.get(`/${viewName}`, desiredMiddleware, function(request, response) {
         response.render(viewName);
     });
 }
 
+//Traverse views directory and actualize any existing views within with the desired middleware passed.
+function tractualizeViews(...desiredMiddleware) {
+    const viewDirectory = join(__dirname, "views");
+    readdir(viewDirectory, function(error, files) {
+        if (error) throw new Error(error);
+        files.forEach(function(file) {
+            if (file.endsWith(".ejs")) {
+                const fileName = file.split(".")[0];
+                actualizeView(fileName, desiredMiddleware);
+            }
+        });
+    });
+}
+
+//Destroy the current existing session with the provided request object.
 function destroySession(request) {
     request.session.destroy(function(error) {
         if (error) throw new Error(error);
