1207a4cc9be2adb97dc6d941fb09701719dfbef0
Charlie <howesybusiness@gmail.com>
2022-06-28 23:22:18 +0100

    Removed views path, actualized index view, removed path module.

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const session = require("express-session");
-const {join} = require("path");
 const { urlencoded, json } = require("body-parser");
 const application = express();
 const specifiedPort = 3000;
 
 application.use("/assets", express.static(__dirname + `/assets`));
-application.use("views", join(__dirname, "views"));
 application.set("view engine", "ejs");
 application.use(urlencoded({ extended: false }));
 application.use(json());
+
+actualizeView("index");
 
 application.listen(specifiedPort, () => console.log("Server successfully initialized."));
 
